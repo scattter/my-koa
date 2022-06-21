a7ae6b4aaa105f33e7c0b32dbd4e1ec8d927f17a
张柯 <413365742@qq.com>
2022-06-21 21:18:05 +0800

    use middleware to catch error

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
 const compose = require('koa-compose');
+const fs = require('fs');
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
 
 const app = new Koa();
 const router = new Router();
-
 
 router.get('/', (ctx, next) => {
   // ctx.router available
@@ -16,25 +18,41 @@ router.get('/login', (ctx, next) => {
   ctx.body = 'login page';
 });
 
-const a1 = (ctx, next) => {
-  console.log('one')
-  next()
-  console.log('one')
-}
+// 使用洋葱模型捕获错误
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (e) {
+    ctx.body = e.message
+  }
+})
 
-const a2 = (ctx, next) => {
-  console.log('two')
-  next()
-  console.log('two')
-}
+app.use(async (ctx, next) => {
+  const data = JSON.parse('{}')
+  // next() 如果不适用async/await 那么就捕获不到错误
+  await next()
+})
 
-const a3 = (ctx, next) => {
-  console.log('three')
-  next()
-  console.log('three')
-}
+app.use(async (ctx, next) => {
+  const data = await readFile('./dd.html')
+  // next() 如果不适用async/await 那么就捕获不到错误
+  ctx.type = 'html'
+  ctx.body = data
+})
 
-app.use(compose([a1, a2, a3]))
+// const a1 = (ctx, next) => {
+//   console.log('one')
+//   next()
+//   console.log('one')
+// }
+//
+// const a2 = (ctx, next) => {
+//   console.log('two')
+//   next()
+//   console.log('two')
+// }
+//
+// app.use(compose([a1, a2]))
 
 app
   .use(router.routes())
