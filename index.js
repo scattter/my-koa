cae3933375f92ab2257391c55c0a1c1f97b44596
张柯 <413365742@qq.com>
2022-06-21 20:44:48 +0800

    add middleware and compose their

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
+const compose = require('koa-compose');
 
 const app = new Koa();
 const router = new Router();
+
 
 router.get('/', (ctx, next) => {
   // ctx.router available
@@ -13,6 +15,26 @@ router.get('/login', (ctx, next) => {
   // ctx.router available
   ctx.body = 'login page';
 });
+
+const a1 = (ctx, next) => {
+  console.log('one')
+  next()
+  console.log('one')
+}
+
+const a2 = (ctx, next) => {
+  console.log('two')
+  next()
+  console.log('two')
+}
+
+const a3 = (ctx, next) => {
+  console.log('three')
+  next()
+  console.log('three')
+}
+
+app.use(compose([a1, a2, a3]))
 
 app
   .use(router.routes())
